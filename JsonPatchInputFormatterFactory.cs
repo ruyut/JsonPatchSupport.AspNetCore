ae812997eb6164044d8f4425ea4675372ab18ab6
Ruyut <a@ruyut.com>
2025-02-18 20:21:42 +0800

    feat: add json patch support with input formatter and setup extension

---
+++ JsonPatchInputFormatterFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace JsonPatchSupport.AspNetCore;
+
+internal static class JsonPatchInputFormatterFactory
+{
+    internal static NewtonsoftJsonPatchInputFormatter GetJsonPatchInputFormatter()
+    {
+        var builder = new ServiceCollection()
+            .AddLogging()
+            .AddMvc()
+            .AddNewtonsoftJson()
+            .Services.BuildServiceProvider();
+
+        return builder
+            .GetRequiredService<IOptions<MvcOptions>>()
+            .Value
+            .InputFormatters
+            .OfType<NewtonsoftJsonPatchInputFormatter>()
+            .First();
+    }
+}
---
+++ JsonPatchSupportSetup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace JsonPatchSupport.AspNetCore;
+
+public static class JsonPatchSupportSetup
+{
+    public static IServiceCollection AddJsonPatchSupport(this IServiceCollection services)
+    {
+        services.AddControllers(options =>
+        {
+            options.InputFormatters.Insert(0, JsonPatchInputFormatterFactory.GetJsonPatchInputFormatter());
+        });
+
+        return services;
+    }
+}
